332a2ba464497cd0223a5a9796b234b172459ddc
Jimmy <jmiller87@gmail.com>
2025-01-16 23:38:39 -0500

    Removed trailing slash. Update batch failed counter

--- src/QueueProcessor.php
+++ src/QueueProcessor.php
@@ -11,7 +11,29 @@ class QueueProcessor
     {
         $job = unserialize($job);
 
-        $result = $job->handle();
+        try {
+            $result = $job->handle();
+        } catch (\Exception $e) {
+            if ($job->batchId) {
+                DB::connection(config('queue.batching.database'))
+                    ->table(config('queue.batching.table'))
+                    ->where('id', '=', $job->batchId)
+                    ->update([
+                        'pending_jobs' => DB::raw('pending_jobs - 1'),
+                        'failed_jobs'  => DB::raw('failed_jobs + 1')
+                    ]);
+
+                DB::connection(config('queue.batching.database'))
+                    ->table(config('queue.batching.table'))
+                    ->where('id', '=', $job->batchId)
+                    ->where('pending_jobs', '=', 0)
+                    ->update([
+                        'finished_at' => now()->timestamp
+                    ]);
+            }
+            throw $e;
+            return;
+        }
 
         if ($job->batchId) {
             DB::connection(config('queue.batching.database'))
